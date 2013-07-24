caffb0badce924ae65a8e7cb9cad4fa43c9a2cbe
adamcoulombe <adamcoulombe187@hotmail.com>
2013-07-24 11:17:08 -0400

    select box support

--- dist/jquery-input-placeholder-text.js
+++ dist/jquery-input-placeholder-text.js
@@ -7,12 +7,27 @@
     return this.each(function(i) {
     	var $self=$(this);
     	var placeholderText=text||$self.data('placeholder-text')||'';
-      
+
       function setPlaceholderText(){
-      	if($self.val()==''){
+      	if($self.val()=='' || $self.val()==placeholderText ){
       		$self.val(placeholderText).addClass('has-placeholder-text')
       	}
       }
+      if($self.is('select')){
+      	setPlaceholderText();
+      	$self.prepend(
+      		$('<option/>').html(placeholderText).val(placeholderText).addClass('placeholder-text-option')
+      	);
+      	$self.change(function(){
+	  		if( $self.val()!=placeholderText){
+	  			$self.removeClass('has-placeholder-text');
+	  		}else{
+	  			setPlaceholderText();
+	  		}
+
+      	});
+
+      }else{
       	setPlaceholderText();
 	  	$self.focus(function(){
 	  		if($self.val()==placeholderText){
@@ -21,6 +36,7 @@
 	  	}).blur(function(){
 	  		setPlaceholderText();
 	  	});
+	  }
 
     });
   };
